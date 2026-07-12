eea2af2bc1608fd6103bb4370d41f33f7f502892
kimuko11 <298197984+kimuko11@users.noreply.github.com>
2026-07-13 01:46:51 +0900

    CSS詳細度をシンプルに

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const キャラエリア一覧 = document.querySelectorAll('.🥸, .🐰, .👩,
 const 監視員 = new IntersectionObserver((項目一覧) => {
   項目一覧.forEach((項目) => {
     if (項目.isIntersecting) {
-      項目.target.querySelector('.キャラ画像').classList.add('表示中');
-      項目.target.querySelector('.💬').classList.add('表示中');
+      項目.target.querySelector('.キャラ画像').classList.add('出現');
+      項目.target.querySelector('.💬').classList.add('出現');
       監視員.unobserve(項目.target); // 一度出たら監視終了（何度も発動させない）
     }
   });
 }, {
-  threshold: 1 // 要素の半分が見えたタイミングで発火
+  threshold: 1 // スタートトリガー (要素がどの程度表示されたか、0～1の範囲で指定)
 });
 
 // 一人ずつ監視対象に登録していく
