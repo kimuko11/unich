66fb46d789ab1cf3f79e2c80b901f84523565419
kimuko11 <298197984+kimuko11@users.noreply.github.com>
2026-07-13 16:31:27 +0900

    速度調整

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const キャラエリア一覧 = document.querySelectorAll('.🥸, .🐰, .👩, .👤');
 
-const 監視員 = new IntersectionObserver((項目一覧) => {
+const 監視 = new IntersectionObserver((項目一覧) => {
   項目一覧.forEach((項目) => {
     if (項目.isIntersecting) {
       項目.target.querySelector('.キャラ画像').classList.add('出現');
       項目.target.querySelector('.💬').classList.add('出現');
-      監視員.unobserve(項目.target); // 一度出たら監視終了（何度も発動させない）
+      監視.unobserve(項目.target); // 一度出現したら監視終了
     }
   });
 }, {
-  threshold: 1 // スタートトリガー (要素がどの程度表示されたか、0～1の範囲で指定)
+  threshold: 1 // 要素がどの程度表示されたら出現するか、0～1の範囲で指定
 });
 
-// 一人ずつ監視対象に登録していく
+// 一つずつ監視対象に登録していく
 キャラエリア一覧.forEach((エリア) => {
-  監視員.observe(エリア);
+  監視.observe(エリア);
 });
