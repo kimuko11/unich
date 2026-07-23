bfb9cc095a21559c9c27faf94dec8bb87e0b07a8
kimuko11 <298197984+kimuko11@users.noreply.github.com>
2026-07-23 17:23:07 +0900

    腕振り・目パチ追加

--- script.js
+++ script.js
@@ -62,14 +62,14 @@ function 音声再生(キャラ) {
     音声.play().catch(() => {}); // 自動再生ブロック対策（無視してOK）
 }
 
-// 🎛️身体・差分アニメ開始
+// 🎛️身体・差分アニメ開始 (モノローグでも動く)
 function 身体アニメ開始(エリア) {
-    エリア.querySelectorAll('.縦伸縮, .横揺れ, .驚きと縦伸縮, .驚き汗').forEach((要素) => {
+    エリア.querySelectorAll('.縦伸縮, .横揺れ, .驚きと縦伸縮, .目玉, .驚き汗').forEach((要素) => {
         要素.classList.add('再生');
     });
 }
 
-// 🎛️口元アニメ開始
+// 🎛️口元アニメ開始 (モノローグでは動かない)
 function 口元アニメ開始(エリア) {
     エリア.querySelectorAll('.口パク').forEach((要素) => {
         要素.classList.add('再生');
